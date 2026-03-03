43d3ebacb192c6d33865016e7f8970407fc4f006
Leandro Lopes <leandro199325@hotmail.com>
2026-03-03 09:33:29 -0300

    criado testes

--- src/main/java/com/hinova/sign/client/CrmClient.java
+++ src/main/java/com/hinova/sign/client/CrmClient.java
@@ -1,21 +1,23 @@
 package com.hinova.sign.client;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
 @Component
-@RequiredArgsConstructor
 public class CrmClient {
+
     private final RestClient restClient;
 
-    @Value("${crm.service.url}")
-    private String crmServiceUrl;
+    public CrmClient(@Value("${crm.service.url}") String crmServiceUrl) {
+        this.restClient = RestClient.builder()
+                .baseUrl(crmServiceUrl)
+                .build();
+    }
 
     public void notificarAssinatura(Long propostaId) {
         restClient.post()
-                .uri(crmServiceUrl + "/api/v1/propostas/callback/assinatura/" + propostaId)
+                .uri("/api/v1/propostas/callback/assinatura/" + propostaId)
                 .retrieve()
                 .toBodilessEntity();
     }
